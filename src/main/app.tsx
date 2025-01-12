60935fed6a234a382d632dae9eff272b10ee3ad6
sunderhus <matheus.sunderhus@gmail.com>
2025-01-12 12:55:42 -0300

    chore: change tost position to top-right

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -7,6 +7,6 @@ import { Router } from "@/main/routes/router";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router />
-    <Toaster richColors closeButton />
+    <Toaster richColors closeButton position="top-right" />
   </React.StrictMode>
 );
